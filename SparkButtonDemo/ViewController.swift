58dd89f9be1dc04b42b9a6b423ac017838009891
Saravanan Vijayakumar <saravanan.v@imaginea.com>
2016-06-04 15:05:36 +0530

    Initial Commit

---
+++ SparkButtonDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SparkButtonDemo
+//
+//  Created by Saravanan on 04/06/16.
+//  Copyright © 2016 Saravanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
